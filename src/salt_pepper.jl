66cef984e1996f345849202e1225f94070b638db
roflmaostc <f.wechsler@tum.de>
2020-04-11 18:07:50 +0200

    Adapt documentation salt pepper

--- src/salt_pepper.jl
+++ src/salt_pepper.jl
@@ -13,12 +13,13 @@ end
 
 
 """
-    salt_pepper(X, [probability, ratio], salt=0.0, pepper=1.0)
+    salt_pepper(X; salt=0.0, pepper=1.0[, probability, ratio])
 
-Introduces salt and pepper noise to the array `X`
-`prob` is the probability that a pixel will be affected
-`salt_prob` is a keyword argument representing the 
-        probability for salt
-`salt` is a keyword argument for specifying the value of salt noise
-`pepper` is a keyword argument for specifying the value of pepper noise
+Returns array `X` affected by salt and pepper noise.
+`prob` is a optional argument for the probability that a pixel will be affected by the noise.
+`salt_prob` is a keyword argument representing the probability for salt noise. 
+The probability for pepper noise is therefore 1-`salt_prob`.
+`salt` is a keyword argument for specifying the value of salt noise.
+`pepper` is a keyword argument for specifying the value of pepper noise.
 """
+salt_pepper
