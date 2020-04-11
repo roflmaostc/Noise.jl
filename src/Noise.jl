570f275c0242b1737ca48603f4c39f9faca5c221
roflmaostc <f.wechsler@tum.de>
2020-04-11 18:10:27 +0200

    Add clip function and gaussian white noise

--- src/Noise.jl
+++ src/Noise.jl
@@ -3,8 +3,13 @@ module Noise
 using PoissonRandom
 using Random
 
+function clip_0_1!(X)
+    map!(x -> max(0.0, min(1.0, x)), X, X)
+    return X
+end
 
 include("poisson.jl")
 include("salt_pepper.jl")
+include("white_noise_additive.jl")
 
 end # module
