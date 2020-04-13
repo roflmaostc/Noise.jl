b75e3b1a75e8790ad37d22214d5725886600483e
roflmaostc <f.wechsler@tum.de>
2020-04-13 22:56:15 +0200

    White noise works for arrays and RGB/Gray images

--- src/Noise.jl
+++ src/Noise.jl
@@ -5,9 +5,15 @@ using Random
 using ColorTypes
 using ImageCore
 
+ # clipping a array
 function clip_0_1!(X)
     map!(x -> max(0.0, min(1.0, x)), X, X)
     return X
+end
+
+ # clipping a single value
+function clip_v(x)
+    return max(0.0, min(1.0, x))
 end
 
 include("poisson.jl")
