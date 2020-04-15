48f652ef45a81b9ae574280c41ade247053f5ce8
roflmaostc <f.wechsler@tum.de>
2020-04-15 13:11:50 +0200

    Refactor poisson

--- src/poisson.jl
+++ src/poisson.jl
@@ -1,80 +1,77 @@
 export poisson
 
+ # get the maximum intensity of an image
+mymax(X::AbstractArray{<:RGB}) = max_rgb(X)
+mymax(X::AbstractArray{<:Gray}) = gray(maximum(X))
+mymax(X::AbstractArray) = maximum(X)
+
+
+ # noise function for poisson
 function noise(x, scaling, max_intens)
     if scaling == Nothing
         return pois_rand(x)
     else
+        # scale image to max_intensity and apply poisson noise
+        # after Poisson noise scale it back
         return pois_rand(x * scaling / max_intens) * max_intens / scaling
     end
 end
 
-
-function poisson(X::Union{AbstractArray{Gray{T}}, AbstractArray{RGB{T}}, AbstractArray{T}}, 
-        scaling=Nothing; clip=false) where {P, T}
-
-    # depending on the element type we either copy or create a new array
-    if eltype(X) <: Union{Gray, RGB}
-        X_noisy = copy(X)
-        # if it's a normed type we should clip it
-        if T <: Normed
-            clip = true
-        end
+ # core function for RGB images which is called from within the loop 
+function poisson_core(a::RGB, max_intens, scaling, clip)
+    if clip
+        return RGB(clip_v(noise(red(a), scaling, max_intens)),
+                         clip_v(noise(green(a), scaling, max_intens)),
+                         clip_v(noise(blue(a), scaling, max_intens)))
     else
-        X_noisy = zeros(size(X))
+        return RGB(noise(red(a), scaling, max_intens),
+                         noise(green(a), scaling, max_intens),
+                         noise(blue(a), scaling, max_intens))
     end
+end
 
-
-    if eltype(X) <: RGB
-        e_type = eltype(red(X[1]))
-        max_intens = max_rgb(X)
-    elseif eltype(X) <: Gray
-        e_type = eltype(gray(X[1]))
-        max_intens = maximum(X)
+ # core for Gray Arrays
+function poisson_core(a::Gray, max_intens, scaling, clip)
+    if clip
+        return Gray(clip_v(noise(gray(a), scaling, max_intens)))
     else
-        max_intens = maximum(X)
+        return Gray(noise(gray(a), scaling, max_intens))
     end
-    
-    
+end
 
-        # scale image to max_intensity and apply poisson noise
-        # after Poisson noise scale it back
-    #= print(noise(0.5), "\n") =#
-    if eltype(X) <: RGB
-        for i in eachindex(X)
-        # for RGB each channel independently
-            a = X[i]
-            if clip
-                X_noisy[i] = RGB(clip_v(noise(red(a), scaling, max_intens)),
-                                 clip_v(noise(green(a), scaling, max_intens)),
-                                 clip_v(noise(blue(a), scaling, max_intens)))
-            else
-                X_noisy[i] = RGB(noise(red(a), scaling, max_intens),
-                                 noise(green(a), scaling, max_intens),
-                                 noise(blue(a), scaling, max_intens))
-            end
-        end
-    # for Gray
-    elseif eltype(X) <: Gray
-        for i in eachindex(X)
-            if clip
-                X_noisy[i] = Gray(clip_v(noise(gray(X[i]), scaling, max_intens)))
-            else
-                X_noisy[i] = Gray(noise(gray(X[i]), scaling, max_intens))
-            end
-        end
+ # core for arbitrary arrays, might fail sometimes
+function poisson_core(a, max_intens, scaling, clip)
+    if clip
+        return clip_v(noise(a, scaling, max_intens))
     else
-        for i in eachindex(X)
-            if clip
-                X_noisy[i] = clip_v(noise(X[i], scaling, max_intens))
-            else
-                X_noisy[i] = noise(X[i], scaling, max_intens)
-            end
-        end
+        return noise(a, scaling, max_intens)
+    end
+end
+
+ # prepares the noise array whether it is copied
+ # if the datatype is Normed, we must clip
+function prepare_poisson(X::Union{AbstractArray{Gray{T}}, AbstractArray{RGB{T}}}, clip) where T
+    clip = T <: Normed ? true : clip
+    return similar(X), clip
+end
+
+ # for arbitrary arrays we simply copy
+function prepare_poisson(X, clip)
+    return zeros(size(X)), clip
+end
+
+function poisson(X::AbstractArray, scaling=Nothing; clip=false)
+    
+    X_noisy, clip = prepare_poisson(X, clip)
+    
+    max_intens = mymax(X)
+    
+    for i in eachindex(X)
+        X_noisy[i] = poisson_core(X[i], max_intens, scaling, clip) 
     end
 
     return X_noisy
 end
-
 
 """
     poisson(X; scaling=1, clip=false)
@@ -83,14 +80,12 @@ Returns the array `X` affected by Poisson noise.
 At every position the Poisson noise affects the intensity individually 
 and the values at the positions represent the expected value of the Poisson
 Distribution. 
-
-`scaling` is a optional argument to scale the highest value of the 
-array to `scaling`. This can be used if the maximum intensity
-corresponds to a number of photons for example.
-
+Since Poisson Noise due to discrete events you should
+provide the optional argument `scaling`. This `scaling` connects
+the highest value of the array with the discrete number of events.
+The highest value will be then scaled and the poisson noise is applied
+Afterwards we scale the whole array back so that the initial intensity
+is preserved but with applied Poisson noise.
 `clip` is a keyword argument. If given, it clips the values to [0, 1]
-
-Please keep in mind that a Poisson Distribution returns integers therefore a image 
-with intensity [0, 1] and scaling=1.0 will only return integers (0, 1, 2, ...).
 """
 poisson
