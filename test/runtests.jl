578da3ff9d9d5c55914fd783674057f6a9b2bb16
roflmaostc <f.wechsler@tum.de>
2020-04-15 15:19:01 +0200

    Add runtest

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,13 +12,28 @@ arr_rand_int = convert(Array{Int64}, map(x -> round(1000*x), arr))
 arr_zeros = zeros(tpl) 
 img_zeros = colorview(RGB, zeros(Normed{UInt8, 8}, 3, 1000, 1000)) 
 img_zeros_gray = colorview(Gray, zeros(Normed{UInt8, 8}, 1000, 1000)) 
+
 img_rand = colorview(RGB, rand(Normed{UInt8, 8}, 3, 1000, 1000)) 
 img_rand_gray = colorview(Gray, rand(Normed{UInt8, 8}, 1000, 1000))
+
 img_12 = colorview(RGB, 0.5 .* ones(Normed{UInt8, 8}, 3, 1000, 1000)) 
 img_12_gray= colorview(Gray, 0.5 .* ones(Normed{UInt8, 8}, 1000, 1000)) 
 
 img = colorview(RGB, ones(Normed{UInt8, 8}, 3, 1000, 1000)) 
 img_gray= colorview(Gray,  ones(Normed{UInt8, 8}, 1000, 1000)) 
+
+img_zeros_float = colorview(RGB, zeros(Normed{UInt8, 8}, 3, 1000, 1000)) 
+img_zeros_gray_float = colorview(Gray, zeros(Normed{UInt8, 8}, 1000, 1000)) 
+
+img_rand_float = colorview(RGB, rand(Float64, 3, 1000, 1000)) 
+img_rand_gray_float = colorview(Gray, rand(Float64, 1000, 1000))
+
+img_12_float = colorview(RGB, 0.5 .* ones(Float64, 3, 1000, 1000)) 
+img_12_gray_float = colorview(Gray, 0.5 .* ones(Float64, 1000, 1000)) 
+
+img_float = colorview(RGB, ones(Float64, 3, 1000, 1000)) 
+img_gray_float = colorview(Gray,  ones(Float64, 1000, 1000)) 
+
 
 
 @testset "Clip functions" begin
@@ -82,6 +97,31 @@ end
     @test abs(std(channelview(salt_pepper(img_rand_gray, 1.0, salt=0.4, pepper=0.5))) -0.05) < 0.001
 
 
+    # check images chn
+    @test abs(mean(channelview(salt_pepper_chn(img_zeros_float, 1.0))) - 0.5)< 0.001 
+    #check that average is different to 0.5 if salt or pepper are not 1.0 or 0.0
+    @test abs(sum(channelview(salt_pepper_chn(img_zeros_float, 0.1, pepper=1.0)) .-0.5)/100/100) > 0.001
+    @test abs(sum(channelview(salt_pepper_chn(img_rand_float, 0.1, salt=0.0)) .-0.5)/100/100) > 0.001
+
+    # check RGB images
+    @test abs(mean(channelview(salt_pepper(img_zeros_float, 1.0))) - 0.5)< 0.001 
+    #check that average is different to 0.5 if salt or pepper are not 1.0 or 0.0
+    @test abs(mean(channelview(salt_pepper(img_zeros_float, 0.1, pepper=1.0)) .-0.5)) > 0.001
+    @test abs(mean(channelview(salt_pepper(img_rand_float, 0.1, salt=0.0)) .-0.5)) > 0.001
+    # check std
+    @test abs(std(channelview(salt_pepper(img_rand_float, 1.0, salt=0.0, pepper=0.5))) -0.25) < 0.001
+    @test abs(std(channelview(salt_pepper(img_rand_float, 1.0, salt=0.4, pepper=0.5))) -0.05) < 0.001
+    
+    # check gray images
+    @test abs(mean(channelview(salt_pepper(img_zeros_gray_float, 1.0))) - 0.5)< 0.001 
+    #check that average is different to 0.5 if salt or pepper are not 1.0 or 0.0
+    @test abs(mean(channelview(salt_pepper(img_zeros_gray_float, 0.1, pepper=1.0)) .-0.5)) > 0.001
+    @test abs(mean(channelview(salt_pepper(img_rand_gray_float, 0.1, salt=0.0)) .-0.5)) > 0.001
+    # check std
+    @test abs(std(channelview(salt_pepper(img_rand_gray_float, 1.0, salt=0.0, pepper=0.5))) -0.25) < 0.001
+    @test abs(std(channelview(salt_pepper(img_rand_gray_float, 1.0, salt=0.4, pepper=0.5))) -0.05) < 0.001
+
+
 end
 
 
@@ -112,6 +152,22 @@ end
     @test abs(mean(channelview(additive_white_gaussian(img_zeros_gray, 0.2, 0.3))) - 0.3) < 0.05
     @test abs(mean(channelview(additive_white_gaussian(img_zeros_gray, 0.0, 10))) - 1.0) < 0.005
 
+    # check images for gaussian white noise
+    # check mean offset channelwise
+    @test abs(mean(channelview(additive_white_gaussian_chn(img_zeros_float, 0.1, 0.5))) - 0.5) < 0.05
+    @test abs(mean(channelview(additive_white_gaussian_chn(img_zeros_float, 0.2, 0.3))) - 0.3) < 0.05
+
+    # check mean offset with clip
+    @test abs(mean(channelview(additive_white_gaussian(img_zeros_float, 0.1, 0.5))) - 0.5) < 0.05
+    @test abs(mean(channelview(additive_white_gaussian(img_zeros_float, 0.2, 0.3))) - 0.3) < 0.05
+    @test abs(mean(channelview(additive_white_gaussian(img_zeros_float, 0.0, 10))) - 1.0) < 0.005
+    
+    # check mean offet with gray
+    @test abs(mean(channelview(additive_white_gaussian(img_zeros_gray_float, 0.1, 0.5))) - 0.5) < 0.05
+    @test abs(mean(channelview(additive_white_gaussian(img_zeros_gray_float, 0.2, 0.3))) - 0.3) < 0.05
+    @test abs(mean(channelview(additive_white_gaussian(img_zeros_gray_float, 0.0, 10))) - 1.0) < 0.005
+
+
 end
 
 @testset "Poisson Noise" begin
@@ -137,4 +193,16 @@ end
     @test abs(std(channelview(poisson(img, 10000.0, clip=true)))) < 0.02
     @test abs(mean(channelview(poisson(img_gray, 10000.0, clip=true)))-1.0)< 0.02
     @test abs(mean(channelview(poisson(img, 10000.0, clip=true))) -1.0) < 0.02
+
+
+    @test abs(std(channelview(poisson(img_12_gray_float, 100.0))) - 0.5 .* sqrt(100.0)./100.0) < 0.005
+    @test abs(std(channelview(poisson(img_12_float, 100.0))) - 0.5 .* sqrt(100.0)./100.0) < 0.005
+    @test abs(std(channelview(poisson(img_12_gray_float, 4200.0))) - 0.3 .* sqrt(4200.0)./4200.0) < 0.005
+    @test abs(std(channelview(poisson(img_12_float, 4200.0))) - 0.3 .* sqrt(4200.0)./4200.0) < 0.005
+    
+    @test abs(std(channelview(poisson(img_gray_float, 10000.0, clip=true))))< 0.02
+    @test abs(std(channelview(poisson(img_float, 10000.0, clip=true)))) < 0.02
+    @test abs(mean(channelview(poisson(img_gray_float, 10000.0, clip=true)))-1.0)< 0.02
+    @test abs(mean(channelview(poisson(img_float, 10000.0, clip=true))) -1.0) < 0.02
+
 end
